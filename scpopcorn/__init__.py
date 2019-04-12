df18a27609ca3c2eb8089b8255d9376cdacbb405
wyjhxq <wangy42@ncbi.nlm.nih.gov>
2019-04-12 09:49:13 -0400

    scpopcorn first version all included

---
+++ scpopcorn/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from scpopcorn.scpopcorn import SingleCellData, MergeSingleCell
+"""Top-level package for PopCorn."""
+
+__author__ = """Yijie Wang"""
+__email__ = 'yijie.wang@nih.gov'
+__version__ = '0.1.0'
